776601d269e7b302179e0669c0a80ee8d1a441ad
Roman A. <aksenov.nsk.r.a@gmail.com>
2024-05-10 11:04:23 +0300

    Add to models Students and Books tables

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, String, Integer, Column
+from sqlalchemy import Boolean, String, Integer, Column, uuid
 
 from database import Base
 
@@ -6,6 +6,29 @@ from database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, unique=True, index=True)
+    id = Column(String, primary_key=True, unique=True)
     name = Column(String, index=True)
     is_active = Column(Boolean, default=True)
+
+
+class Students(Base):
+    __tablename__ = "Student"
+
+    student_uid = Column(uuid, primary_key=True, unique=True)
+
+    student_name = Column(String, index=True)
+    student_class = Column(String, index=True)
+
+
+class Books(Base):
+    __tablename__ = "Books"
+
+    book_uid = Column(uuid, primary_key=True, unique=True)
+
+    library_id = Column(String, index=True)
+    book_name = Column(String, index=True)
+    book_author = Column(String, index=True)
+    book_description = Column(String, index=True)
+
+    available = Column(Boolean, default=True)
+    student_uid = Column(uuid, primary_key=True, unique=True, default=None)
