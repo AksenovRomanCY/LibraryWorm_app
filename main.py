1500b6461b7ff92621136421d615e383ac63cd0e
Roman A. <aksenov.nsk.r.a@gmail.com>
2024-05-10 21:05:11 +0300

    Link main and routers

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 app.include_router(router_user.router, prefix="/user")
+app.include_router(router_user.router, prefix="/students")
+app.include_router(router_user.router, prefix="/books")
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host='127.0.0.1', port=8000, reload=True, workers=3)
